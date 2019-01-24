d22763ef2f3e4b9887cef09acd20c5f957db70f4
Leonardo S. Senna <leoschsenna@gmail.com>
2019-01-23 22:00:51 -0200

    Criado módulo para erros customizados

---
+++ erros/erros.go
@@ -0,0 +1,54 @@
+// Package erros disponibiliza uma nova estrutura de erros que implementa a
+// interface "error". A vantagem dessa estrutura é a presença de uma flag que
+// indica se o erro é interno do servidor e deve ser registrado em logs de erros
+// para análise e debug, ou se o erro é externo por parte do cliente, caso no
+// qual o erro não deve ser registrado em logs, e um código apropriado deve ser
+// enviado para o cliente tratar e apresentar o erro apropriadamente ao usuário
+package erros
+
+import (
+	"errors"
+	"log"
+)
+
+// Erros é a estrutura que deve ser usada como objeto que implementa a interface
+// 'erro' nos retornos de funções
+type Erros struct {
+	// interno se o erro é do servidor (true) ou se é problema de operação
+	// (false)
+	interno bool
+	// msg é a mensagem que erros têm por padrão
+	// Se o erro for interno, recomenda-se que se utilize essa variável do mesmo
+	// jeito que se utilizaria a mensagem de erros com as funções padrões do
+	// Golang. Se o erro for externo, recomenda-se que se utilize um código ou
+	// identificador pequeno para ser passado para o cliente, que identificará o
+	// código e tratará o erro adequadamente, construindo por si mesmo a
+	// mensagem de erro apropriada
+	msg string
+	// statusCode armazena o status code segundo o protocolo HTTP que a
+	// resposta HTTP deve ter graças à presença do erro gerado
+	statusCode int
+}
+
+// Error é a função para a estrutura 'Erros' implementar a interface 'error'
+func (e Erros) Error() string {
+	return e.msg
+}
+
+// Abre é a função a ser chamada na hora de tratar o erro. Essa função registra
+// o erro no log de erros (no caso, utilizando o pacote 'log', então a mensagem
+// vai para o stderr) e retorna, além de seus dados extras (variáveis 'interno'
+// e 'statusCode'), um objeto 'error' gerado pelo package padrão do Golang com
+// a mensagem de erro idêntica à do objeto.
+func (e Erros) Abre() (bool, int, error) {
+	if e.interno {
+		log.Print(e)
+	}
+	return e.interno, e.statusCode, errors.New(e.msg)
+}
+
+// Padrao retorna o erro gerado pela função 'errors.New' com a mesma mensagem
+// que a sua própria
+func (e Erros) Padrao() error {
+	return errors.New(e.msg)
+}
---
+++ erros/erros_test.go
@@ -0,0 +1,71 @@
+package erros
+
+import (
+	"bytes"
+	"errors"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestError(t *testing.T) {
+	msg := "mensagem de teste de erro"
+	e := Erros{interno: true, statusCode: 200, msg: msg}
+	msgRecebida := e.Error()
+
+	if msgRecebida != msg {
+		t.Errorf("Mensagem esperada: %v\nObtida: %v", msg, msgRecebida)
+	}
+}
+
+func TestAbreInterno(t *testing.T) {
+	buf := testAbre(t, true)
+	if buf.String() == "" {
+		t.Errorf("Logging de erro incorreto. Log adquirido: %v", buf.String())
+	}
+}
+
+func TestAbreExterno(t *testing.T) {
+	buf := testAbre(t, false)
+	if buf.String() != "" {
+		t.Errorf("Logging não deveria ocorrer. Log adquirido: %v", buf.String())
+	}
+}
+
+func TestPadrao(t *testing.T) {
+	erroNormal := errors.New("mensagem de teste de erro")
+	e := Erros{interno: true, statusCode: 200, msg: erroNormal.Error()}
+	erroRecebido := e.Padrao()
+
+	if erroRecebido.Error() != erroNormal.Error() {
+		t.Logf("Esperado: %v\nObtido: %v", erroNormal, erroRecebido)
+		t.Fail()
+	}
+}
+
+// testAbre é a função base para os testes da função Abre. Outras funções de
+// teste da função Abre podem derivar dessa função
+func testAbre(t *testing.T, interno bool) bytes.Buffer {
+	// Código para lermos o log de erros gerado pela função
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() { log.SetOutput(os.Stderr) }()
+
+	erroNormal := errors.New("mensagem de teste de erro")
+	e := Erros{interno: interno, statusCode: 200, msg: erroNormal.Error()}
+	internoRecebido, statusCode, erroRecebido := e.Abre()
+
+	if erroRecebido.Error() != erroNormal.Error() {
+		t.Errorf("Mensagem esperada: %v\nObtida: %v", erroNormal, erroRecebido)
+	}
+	if internoRecebido != interno {
+		t.Errorf("Valor de 'interno' incorreto (%v, deveria ser %v)",
+			internoRecebido, interno)
+	}
+	if statusCode != 200 {
+		t.Errorf("Valor de 'statusCode' incorreto (deveria ser %v, foi %v)",
+			200, statusCode)
+	}
+
+	return buf
+}
