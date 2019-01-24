3a0175d2dbf95c5c7f36a963ae05427484aea9fc
Leonardo S. Senna <leoschsenna@gmail.com>
2019-01-23 23:48:15 -0200

    Criada abstração para validação simples com regex

--- cadastro/campos.go
+++ cadastro/campos.go
@@ -8,24 +8,25 @@ import (
 
 // email verifica se o e-mail é válido (formato regex /.+@.+/)
 func email(email string) error {
-	matched, err := regexp.MatchString(".+@.+", email)
-	if err != nil {
-		return erros.CriaInternoPadrao(err)
-	}
-	if !matched {
-		return ErrEmailInvalido
-	}
-	return nil
+	return validacaoMatchSimples(email, "^.+@.+$", ErrEmailInvalido)
 }
 
 // senha verifica se a senha possui pelo menos 6 caracteres e no máximo 64
 func senha(senha string) error {
-	matched, err := regexp.MatchString("^.{6,64}$", senha)
+	return validacaoMatchSimples(senha, "^.{6,64}$", ErrSenhaInvalida)
+}
+
+// validacaoMatchSimples executa uma validação básica com um regex passado como
+// argumento. Retorna o erro gerado pela função do regex, caso houve algum, ou o
+// erro passado por argumento para essa função no caso de o regex não bater com
+// o dado passado.
+func validacaoMatchSimples(s string, reg string, e error) error {
+	matched, err := regexp.MatchString(reg, s)
 	if err != nil {
 		return erros.CriaInternoPadrao(err)
 	}
 	if !matched {
-		return ErrSenhaInvalida
+		return e
 	}
 	return nil
 }
