feb7896073a4ddd35bef108e33e8dc930ce0da66
Leonardo S. Senna <leoschsenna@gmail.com>
2019-01-30 01:58:34 -0200

    Ajustado módulo 'cadastro' para utilizar corretamente o módulo de erros

--- cadastro/cadastro.go
+++ cadastro/cadastro.go
@@ -33,16 +33,16 @@ type dadosCadastrais struct {
 // RealizaCadastroRequestHTTP realiza o cadastro de um usuário a partir de um
 // request HTTP. O request e os dados do usuário serão validados com a função
 // ValidaDadosCadastroRequestHTTP.
-func RealizaCadastroRequestHTTP(r *http.Request) error {
+func RealizaCadastroRequestHTTP(r *http.Request) erros.Erros {
 	// Valida o usuário
 	dados, err := validaDadosCadastroRequestHTTP(r)
-	if err != nil {
+	if !erros.Vazio(err) {
 		return err
 	}
 	// Gera a senha hashed
-	senhaHashed, err := passenc.GeraHashed([]byte(dados.senha))
-	if err != nil {
-		return err
+	senhaHashed, err2 := passenc.GeraHashed([]byte(dados.senha))
+	if err2 != nil {
+		return erros.CriaInternoPadrao(err2)
 	}
 	// Insere o usuário no banco de dados
 	usr := database.Usuario{
@@ -52,17 +52,17 @@ func RealizaCadastroRequestHTTP(r *http.Request) error {
 		Nascimento: dados.nascimento,
 	}
 	if err := database.InsereUsuario(&usr); err != nil {
-		return err
+		return erros.CriaInternoPadrao(err)
 	}
-	return nil
+	return erros.CriaVazio()
 }
 
 // VerificaLoginRequestHTTP verifica se o usuário existe e a senha está correta
 // a partir de um request HTTP. O request deve ser do tipo POST.
-func VerificaLoginRequestHTTP(r *http.Request) (bool, error) {
+func VerificaLoginRequestHTTP(r *http.Request) (bool, erros.Erros) {
 	// Adquire os dados do request
 	if err := comunicacao.RealizaParseForm(r); err != nil {
-		return false, err
+		return false, erros.CriaInternoPadrao(err)
 	}
 	email := r.PostFormValue("email")
 	senha := r.PostFormValue("senha")
@@ -74,55 +74,49 @@ func VerificaLoginRequestHTTP(r *http.Request) (bool, error) {
 // recebidos no request HTTP. O request deve ser do tipo POST. Após adquirir os
 // dados do request, a função chama validaDadosCadastro para validar e limpar os
 // dados
-func validaDadosCadastroRequestHTTP(r *http.Request) (dados dadosCadastrais, err error) {
+func validaDadosCadastroRequestHTTP(r *http.Request) (dadosCadastrais, erros.Erros) {
 	// Adquire os dados do request
 	if err := comunicacao.RealizaParseForm(r); err != nil {
-		return dadosCadastrais{}, err
+		return dadosCadastrais{}, erros.CriaInternoPadrao(err)
 	}
+	var dados dadosCadastrais
 	dados.email = r.PostFormValue("email")
 	dados.senha = r.PostFormValue("senha")
 	dados.nome = r.PostFormValue("nome")
 	dados.nascimento = r.PostFormValue("nascimento")
 	// Validação e retorno
-	err = validaDadosCadastro(&dados)
-	return
+	err := validaDadosCadastro(&dados)
+	return dados, err
 }
 
 // validaDadosCadastro verifica todos os dados recebidos em 'dados'. Se for
 // possível formatar corretamente e tornar os dados válidos, isso será feito. Se
 // houver algum erro incorrigível com os dados, a função retorna o status code e
 // mensagem de erros apropriados.
-func validaDadosCadastro(dados *dadosCadastrais) (err error) {
-	if err := email(dados.email); err != nil {
-		return err
-	}
-	if err := senha(dados.senha); err != nil {
-		return err
-	}
-	if err := nome(dados.nome); err != nil {
-		return err
-	}
-	if err := nascimento(dados.nascimento); err != nil {
-		return err
-	}
-	return
+func validaDadosCadastro(dados *dadosCadastrais) erros.Erros {
+	err := erros.CriaVazio()
+	err = erros.JuntaErros(err, email(dados.email))
+	err = erros.JuntaErros(err, senha(dados.senha))
+	err = erros.JuntaErros(err, nome(dados.nome))
+	err = erros.JuntaErros(err, nascimento(dados.nascimento))
+	return err
 }
 
 // verificaLogin verifica se os credenciais existem e estão corretos no banco
 // de dados utilizando encriptação de senhas
-func verificaLogin(email string, senha string) (bool, error) {
+func verificaLogin(email string, senha string) (bool, erros.Erros) {
 	// Adquire a senha hashed do banco de dados
 	senhaDB, err := database.AdquireSenhaHashed(email)
 	if err == database.ErrUsuarioNaoExiste {
-		return false, nil
+		return false, erros.CriaVazio()
 	} else if err != nil {
-		return false, err
+		return false, erros.CriaInternoPadrao(err)
 	}
 
 	// Verifica se as senhas são as mesmas
 	sucesso, err := passenc.VerificaSenha([]byte(senha), senhaDB)
 	if err != nil {
-		return false, err
+		return false, erros.CriaInternoPadrao(err)
 	}
-	return sucesso, nil
+	return sucesso, erros.CriaVazio()
 }
--- cadastro/campos.go
+++ cadastro/campos.go
@@ -10,7 +10,7 @@ import (
 
 // email verifica se o e-mail é válido (formato regex /.+@.+/) e possui pelo
 // menos 3 caracteres e no máximo 128 caracteres
-func email(email string) error {
+func email(email string) erros.Erros {
 	if utf8.RuneCountInString(email) < 3 || utf8.RuneCountInString(email) > 128 {
 		return ErrNomeInvalido
 	}
@@ -18,7 +18,7 @@ func email(email string) error {
 }
 
 // senha verifica se a senha possui pelo menos 6 caracteres e no máximo 50 bytes
-func senha(senha string) error {
+func senha(senha string) erros.Erros {
 	if len([]byte(senha)) > 50 {
 		return ErrSenhaMuitoLonga
 	}
@@ -26,11 +26,11 @@ func senha(senha string) error {
 }
 
 // nome verifica se o campo não está vazio e se não excede 128 caracteres
-func nome(nome string) error {
+func nome(nome string) erros.Erros {
 	if utf8.RuneCountInString(nome) <= 0 || utf8.RuneCountInString(nome) > 128 {
 		return ErrNomeInvalido
 	}
-	return nil
+	return erros.CriaVazio()
 }
 
 // nascimento verifica se a data está no formato válido (YYYY-MM-DD) e se a data
@@ -41,7 +41,7 @@ func nome(nome string) error {
 // nessa função, mas essa margem é pequena (algumas horas, possivelmente alguns
 // dias, dependendo de mudanças específicas de fusos horários) e pode ser
 // ignorada.
-func nascimento(data string) error {
+func nascimento(data string) erros.Erros {
 	dataTime, err := time.Parse("2006-01-02", data)
 	if err != nil {
 		return ErrNascimentoInvalido
@@ -50,14 +50,14 @@ func nascimento(data string) error {
 	if dataTime.After(agora) {
 		return ErrNascimentoInvalido
 	}
-	return nil
+	return erros.CriaVazio()
 }
 
 // validacaoMatchSimples executa uma validação básica com um regex passado como
 // argumento. Retorna o erro gerado pela função do regex, caso houve algum, ou o
 // erro passado por argumento para essa função no caso de o regex não bater com
 // o dado passado.
-func validacaoMatchSimples(s string, reg string, e error) error {
+func validacaoMatchSimples(s string, reg string, e erros.Erros) erros.Erros {
 	matched, err := regexp.MatchString(reg, s)
 	if err != nil {
 		return erros.CriaInternoPadrao(err)
@@ -65,5 +65,5 @@ func validacaoMatchSimples(s string, reg string, e error) error {
 	if !matched {
 		return e
 	}
-	return nil
+	return erros.CriaVazio()
 }
